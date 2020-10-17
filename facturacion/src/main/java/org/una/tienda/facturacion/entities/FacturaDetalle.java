fd2bcb736617fec898baf32eea8be82bf2d63ea3
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2020-10-16 18:40:23 -0600

    Update FacturaDetalle.java

--- facturacion/src/main/java/org/una/tienda/facturacion/entities/FacturaDetalle.java
+++ facturacion/src/main/java/org/una/tienda/facturacion/entities/FacturaDetalle.java
@@ -5,10 +5,81 @@
  */
 package org.una.tienda.facturacion.entities;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  *
  * @author colo7
  */
+@Entity
+@Table(name = "ut_facturas_detalles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class FacturaDetalle {
+    
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+
+    @Column
+    private double cantidad;
+    
+
+    @Column(name="descuento_Final")
+    private double descuentoFinal;
+ 
+    @Column
+    private boolean estado;
+    
+    @Column(name = "fecha_registro", updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @Setter(AccessLevel.NONE)
+    private Date fechaRegistro;
+
+    @Column(name = "fecha_modificacion")
+    @Setter(AccessLevel.NONE)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaModificacion;
+    
+    @Column( name = "factura_id")
+    private Factura facturaId;
+    
+    @Column( name = "producto_id")
+    private Producto productoId;
+    
+    
+      private static final long serialVersionUID = 1L;
+
+    @PrePersist
+    public void prePersist() {
+        estado = true;
+        fechaRegistro = new Date();
+        fechaModificacion = new Date();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        fechaModificacion = new Date();
+    }
     
 }
