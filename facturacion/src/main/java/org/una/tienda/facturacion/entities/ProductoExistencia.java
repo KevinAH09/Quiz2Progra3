89e17c03fd3e2dce2ba0efca7d3b174abeec1d72
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2020-10-16 18:32:39 -0600

    Update ProductoExistencia.java

---
+++ facturacion/src/main/java/org/una/tienda/facturacion/entities/ProductoExistencia.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.tienda.facturacion.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ *
+ * @author colo7
+ */
+@Entity
+@Table(name = "ut_productos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ProductoExistencia {
+    
+}
