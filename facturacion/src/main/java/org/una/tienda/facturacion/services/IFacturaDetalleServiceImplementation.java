2b3511b1fb8a4c1098be12a94d3584f5e308b8c8
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2020-10-18 21:18:50 -0600

    Update IFacturaDetalleServiceImplementation.java

--- facturacion/src/main/java/org/una/tienda/facturacion/services/IFacturaDetalleServiceImplementation.java
+++ facturacion/src/main/java/org/una/tienda/facturacion/services/IFacturaDetalleServiceImplementation.java
@@ -7,8 +7,6 @@ package org.una.tienda.facturacion.services;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +14,6 @@ import org.una.tienda.facturacion.dtos.FacturaDetalleDTO;
 import org.una.tienda.facturacion.dtos.ProductoExistenciaDTO;
 import org.una.tienda.facturacion.dtos.ProductoPrecioDTO;
 import org.una.tienda.facturacion.entities.FacturaDetalle;
-import org.una.tienda.facturacion.entities.ProductoExistencia;
 import org.una.tienda.facturacion.exceptions.NoCrearFacturaConProductoPrecioCeroException;
 import org.una.tienda.facturacion.exceptions.NoCrearFacturasConCantidadCeroException;
 import org.una.tienda.facturacion.exceptions.NoCrearFacturasConProductoInventarioCeroOMenorException;
@@ -59,34 +56,29 @@ public class IFacturaDetalleServiceImplementation implements IFacturaDetalleServ
 
     @Override
     @Transactional
-    public FacturaDetalleDTO create(FacturaDetalleDTO facturaDetalle) throws ProductoConDescuentoMayorAlPermitidoException,NoCrearFacturaConProductoPrecioCeroException,NoCrearFacturasConCantidadCeroException, NoCrearFacturasConProductoInventarioCeroOMenorException {
+    public FacturaDetalleDTO create(FacturaDetalleDTO facturaDetalle) throws ProductoConDescuentoMayorAlPermitidoException, NoCrearFacturaConProductoPrecioCeroException, NoCrearFacturasConCantidadCeroException, NoCrearFacturasConProductoInventarioCeroOMenorException {
 
         Optional<ProductoPrecioDTO> productoPrecio = productoPrecioService.findByProductoId(facturaDetalle.getProductoId().getId());
-        
-         Optional<ProductoExistenciaDTO> productoExistencia = productoExistenciaService.findByProductoId(facturaDetalle.getProductoId().getId());
-System.out.println(productoExistencia);
+
+        Optional<ProductoExistenciaDTO> productoExistencia = productoExistenciaService.findByProductoId(facturaDetalle.getProductoId().getId());
         if (productoPrecio.isEmpty()) {
-            System.out.println("org.una.tienda.facturacion-CREATE -------------- ES NULOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
             return null;
         }
 
-        if (productoPrecio.get().getPrecioColones()== Double.valueOf("0.0")) {
+        if (productoPrecio.get().getPrecioColones() == Double.valueOf("0.0")) {
 
             throw new NoCrearFacturaConProductoPrecioCeroException("Para que no se facture sobre productos con precio cero");
 
         }
-        
-        if(Double.valueOf("0.0") >= productoExistencia.get().getCantidad())
-        {
+
+        if (Double.valueOf("0.0") >= productoExistencia.get().getCantidad()) {
             throw new NoCrearFacturasConProductoInventarioCeroOMenorException("Para que no se facture sobre productos con inventario cero o menor");
         }
-        
-        if(facturaDetalle.getCantidad()==Double.valueOf("0.0"))
-        {
-             throw new NoCrearFacturasConCantidadCeroException("Para que no se facture sobre productos con cantidad cero");
+
+        if (facturaDetalle.getCantidad() == Double.valueOf("0.0")) {
+            throw new NoCrearFacturasConCantidadCeroException("Para que no se facture sobre productos con cantidad cero");
         }
 
-        
         if (facturaDetalle.getDescuentoFinal() > productoPrecio.get().getDescuentoMaximo()) {
             throw new ProductoConDescuentoMayorAlPermitidoException("Se intenta facturar un producto con un descuento mayor al permitido");
         }
@@ -104,7 +96,6 @@ System.out.println(productoExistencia);
         if (factura.isEmpty()) {
             return null;
         }
-        System.out.println(factura);
         if (factura.get().isEstado() == false) {
             throw new NoModificarInformacionFacturaDetalleConEstadoInactivoException("Se intenta modificar una factura detalle con un estado inactivo");
         }
